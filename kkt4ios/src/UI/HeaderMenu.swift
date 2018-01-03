dfc928d93dec8316df94dc2de3e57b8829d45f7f
gomachan_7 <tt@a-azarashi.jp>
2018-01-03 11:40:19 +0900

    Add HeaderMenu skelton

---
+++ kkt4ios/src/UI/HeaderMenu.swift
@@ -0,0 +1,39 @@
+//
+//  HeaderMenu.swift
+//  kkt4ios
+//
+//  Created by tt on 2018/01/03.
+//  Copyright © 2018年 gomachan_7. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HeaderMenu: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame);
+        self.loadNib()
+        self.setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+        self.loadNib()
+        self.setupView()
+    }
+    
+    // MARK:- private
+    
+    private func loadNib(){
+        let view = Bundle.main.loadNibNamed("HeaderMenu", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    private func setupView() {
+        if let bgImage = UIImage(named: "bg_header.png") {
+            self.backgroundColor = UIColor(patternImage: bgImage)
+        }
+    }
+}
+
