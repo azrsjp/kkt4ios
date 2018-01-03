810e1413c1a5e123384f22e156cbf60bb993edcc
gomachan_7 <tt@a-azarashi.jp>
2018-01-03 16:49:00 +0900

    Add skeleton DrawerViewController

---
+++ kkt4ios/src/View/DrawerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DrawerViewController.swift
+//  kkt4ios
+//
+//  Created by tt on 2018/01/03.
+//  Copyright © 2018年 gomachan_7. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class DrawerViewController: PagedViewControllerBase {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+    }
+}
+
