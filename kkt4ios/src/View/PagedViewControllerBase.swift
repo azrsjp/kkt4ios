4b69ed3f153e54ec25ef045997c67a459ffb826f
gomachan_7 <tt@a-azarashi.jp>
2018-01-03 15:15:33 +0900

    Add PagedViewController for XLPagerTabStrip managed VC

---
+++ kkt4ios/src/View/PagedViewControllerBase.swift
@@ -0,0 +1,23 @@
+//
+//  PagedViewControllerBase.swift
+//  kkt4ios
+//
+//  Created by tt on 2018/01/03.
+//  Copyright © 2018年 gomachan_7. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import XLPagerTabStrip
+
+class PagedViewControllerBase: UIViewController, IndicatorInfoProvider {
+    
+    private var itemInfo: IndicatorInfo = ""
+    
+    // MARK: - IndicatorInfoProvider
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return itemInfo
+    }
+}
+
