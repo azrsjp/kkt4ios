ef0254670208ad1a10aa053da2bf5eb9cd0e155e
gomachan_7 <tt@a-azarashi.jp>
2018-01-14 22:59:11 +0900

    Extend RxSwift with asCompletable

---
+++ kkt4ios/src/Core/Extensions/RxSwift+asCompletable.swift
@@ -0,0 +1,29 @@
+//
+//  RxSwift+asCompletable.swift
+//  kkt4ios
+//
+//  Created by tt on 2018/01/14.
+//  Copyright © 2018年 gomachan_7. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+extension ObservableType {
+    func asCompletable() -> Completable {
+        return ignoreElements()
+    }
+}
+
+extension PrimitiveSequence where Trait == SingleTrait {
+    func asCompletable() -> Completable {
+        return asObservable().asCompletable()
+    }
+}
+
+extension PrimitiveSequence where Trait == MaybeTrait {
+    func asCompletable() -> Completable {
+        return asObservable().asCompletable()
+    }
+}
