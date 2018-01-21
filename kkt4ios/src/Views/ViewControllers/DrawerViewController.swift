decf57b155dbeaa4c03018e5f0ea8195f796e294
gomachan_7 <tt@a-azarashi.jp>
2018-01-21 23:44:42 +0900

    Modify DrawerVC to put logout button

--- kkt4ios/src/Views/ViewControllers/DrawerViewController.swift
+++ kkt4ios/src/Views/ViewControllers/DrawerViewController.swift
@@ -6,12 +6,56 @@
 //  Copyright © 2018年 gomachan_7. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 import XLPagerTabStrip
 
 class DrawerViewController: PagedViewControllerBase {
+
+    @IBOutlet var logoutButton: UIButton!
+
+    private let disposeBag = DisposeBag()
+    private let drawerViewModel = DrawerViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
+
+        bindInput()
+        bindOutput()
+    }
+
+    // MARK: - private
+
+    private func moveToLogin() {
+        let loginVC = LoginViewController()
+
+        UIApplication.shared.keyWindow?.rootViewController = loginVC
+    }
+
+    private func bindInput() {
+        drawerViewModel.outputs
+            .isLoading
+            .asDriver()
+            .drive(loadingView)
+            .disposed(by: disposeBag)
+
+        drawerViewModel.outputs
+            .moveToLogin
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] in
+                self.moveToLogin()
+            }).disposed(by: disposeBag)
+    }
+
+    private func bindOutput() {
+        logoutButton
+            .rx.tap
+            .asDriver()
+            .drive(onNext: { [unowned self] in
+                self.drawerViewModel.logout()
+            })
+            .disposed(by: disposeBag)
     }
 }
