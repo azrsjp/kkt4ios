ec6e29acf5cb9750f846801f9503a0a2386c320e
gomachan_7 <tt@a-azarashi.jp>
2018-01-21 23:46:39 +0900

    Add Utility functions

---
+++ kkt4ios/src/Core/Utility.swift
@@ -0,0 +1,16 @@
+//
+//  Utility.swift
+//  kkt4ios
+//
+//  Created by tt on 2018/01/21.
+//  Copyright © 2018年 gomachan_7. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Utility {
+    static func statusBarHeight() -> CGFloat {
+        return UIApplication.shared.statusBarFrame.size.height
+    }
+}
